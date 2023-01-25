dd170780209c127388fc9e791b1dc1c77cec0845
HannaFox <h_weidemann@gmx.de>
2023-01-25 17:24:06 +0100

    labs

--- 15_Lab_SQL_joins_on_multiple_tables.sql
+++ 15_Lab_SQL_joins_on_multiple_tables.sql
@@ -13,49 +13,47 @@ JOIN sakila.payment using (staff_id)
 GROUP BY store_id;
 
 # 3) What is the average running time(length) of films by category?
-Expected output:
+SELECT * from sakila.category;
+SELECT name, avg(`length`) from sakila.category
+JOIN sakila.film_category using (category_id)
+JOIN sakila.film using (film_id)
+GROUP BY name
+ORDER BY name;
 
-Action		111.61
-Animation	111.02
-Children	109.80
-Classics	111.67
-Comedy	115.83
-Documentary	108.75
-Drama	120.84
-Family		114.78
-Foreign	121.70
-Games	127.84
-Horror		112.48
-Music		113.65
-New		111.13
-Sci-Fi		108.20
-Sports		128.20
-Travel		113.32
-Which film categories are longest(length) (find Top 5)? (Hint: You can rely on question 3 output.)
-Expected output:
+# 4) Which film categories are longest(length) (find Top 5)? (Hint: You can rely on question 3 output.)
+SELECT name, avg(`length`) from sakila.category
+JOIN sakila.film_category using (category_id)
+JOIN sakila.film using (film_id)
+GROUP BY name
+ORDER BY avg(`length`) DESC;
 
-Sports		128.20
-Games	127.84
-Foreign	121.70
-Drama	120.84
-Comedy	115.83
-Display the top 5 most frequently(number of times) rented movies in descending order.
-Expected output:
+# 5) Display the top 5 most frequently(number of times) rented movies in descending order.
+SELECT title, count(return_date) from sakila.rental
+JOIN sakila.inventory using (inventory_id)
+JOIN sakila.film using (film_id)
+GROUP BY title
+ORDER BY count(return_date) DESC
+LIMIT 5;
 
-BUCKET BROTHERHOOD
-ROCKETEER MOTHER
-FORWARD TEMPLE
-GRIT CLOCKWORK
-JUGGLER HARDLY
-List the top five genres in gross revenue in descending order.
-Expected output:
 
-Sports		5314.21
-Sci-Fi		4756.98
-Animation	4656.30
-Drama	4587.39
-Comedy	4383.58
-Is "Academy Dinosaur" available for rent from Store 1?
-Expected output:
+# 6) List the top five genres in gross revenue in descending order.
+SELECT * from sakila.film;
+SELECT name "genre", sum(amount) "amount" 
+FROM sakila.category
+JOIN sakila.film_category using (category_id)
+JOIN sakila.film using (film_id)
+JOIN sakila.inventory using (film_id)
+JOIN sakila.rental using (inventory_id)
+JOIN sakila.payment using (rental_id)
+GROUP BY genre
+ORDER BY amount desc
+LIMIT 5;
 
-ACADEMY DINOSAUR	1
+# 7) Is "Academy Dinosaur" available for rent from Store 1?
+SELECT * from sakila.inventory;
+
+SELECT title, store_id from sakila.store
+JOIN sakila.inventory using (store_id)
+JOIN sakila.film using (film_id)
+WHERE title = "Academy Dinosaur" AND store_id = 1
+GROUP BY title;
---
+++ 16_Lab_SQL_Subqueries.sql
@@ -0,0 +1,83 @@
+USE sakila;
+SELECT first_name, last_name FROM actor
+WHERE actor_id in (SELECT actor_id FROM film_actor
+					WHERE film_id = 1);
+                    
+SELECT first_name, last_name FROM actor
+WHERE actor_id in (SELECT actor_id FROM film_actor
+					WHERE film_id = (SELECT film_id FROM film
+									WHERE title = "Academy Dinosaur"));
+
+# 1) List all films whose length is longer than the average of all the films.
+SELECT title FROM film
+WHERE length > (SELECT avg(length) FROM film);
+
+# 2) How many copies of the film Hunchback Impossible exist in the inventory system?
+SELECT count(inventory_ID) from inventory
+WHERE film_ID = (SELECT film_ID from film
+				WHERE title = "Hunchback Impossible");
+
+# 3) Use subqueries to display all actors who appear in the film Alone Trip.
+SELECT first_name, last_name from actor
+WHERE actor_ID in (SELECT actor_ID from film_actor
+					WHERE film_ID = (SELECT film_ID from film
+					WHERE title = "Alone Trip"));
+                    
+# 4) Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as family films.
+SELECT title from film
+WHERE film_ID in (SELECT film_ID from film_category
+					WHERE category_ID in (SELECT category_ID from category
+					WHERE name = "Family"));
+
+# 5) Get name and email from customers from Canada using subqueries. Do the same with joins. Note that to create a join, you will have to identify the correct tables with their primary keys and foreign keys, that will help you get the relevant information.
+SELECT first_name, last_name, email from customer
+WHERE address_ID in (SELECT address_ID from address
+					WHERE city_ID in (SELECT city_ID from city
+					WHERE country_ID in (SELECT country_ID from country
+                    WHERE country = "Canada")));
+
+# 6) Optional: Which are films starred by the most prolific actor? Most prolific actor is defined as the actor that has acted in the most number of films. First you will have to find the most prolific actor and then use that actor_id to find the different films that he/she starred.
+
+SELECT title from film
+WHERE film_id in (SELECT film_id from film_actor
+					WHERE actor_id = (SELECT actor_ID from film_actor
+                    GROUP BY actor_id ORDER BY count(film_id) DESC LIMIT 1));
+
+SELECT title, actor_id from film
+JOIN film_actor USING (film_ID)
+JOIN actor USING (actor_ID)
+GROUP BY actor_id
+ORDER BY count(film_ID) DESC LIMIT 1;
+
+
+
+# 7)Films rented by most profitable customer. You can use the customer table and payment table to find the most profitable customer ie the customer that has made the largest sum of payments
+SELECT title from film;
+WHERE film_id in (SELECT film_id from film_actor
+					WHERE actor_id = (SELECT actor_ID from film_actor
+                    GROUP BY actor_id ORDER BY count(film_id) DESC LIMIT 1));
+
+Expected output:
+
+44 rows including 
+DESTINY SATURDAY
+CYCLONE FAMILY
+SLUMS DUCK
+FIDELITY DEVIL
+SPLASH GUMP
+MISSION ZOOLANDER
+MULHOLLAND BEAST
+PRINCESS GIANT
+PARIS WEEKEND
+RACER EGG
+Customers who spent more than the average payments(this refers to the average of all amount spent per each customer).
+Expected output:
+
+285 rows including:
+ALAN KAHN
+ALEX GRESHAM
+ALEXANDER FENNELL
+…
+WARREN SHERROD
+WESLEY BULL
+ZACHARY HITE
