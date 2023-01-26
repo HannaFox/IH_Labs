b1b170bffbede0c1927e21728a03d2b7745a8eb7
HannaFox <h_weidemann@gmx.de>
2023-01-26 23:50:19 +0100

    labs

--- 16_Lab_SQL_Subqueries.sql
+++ 16_Lab_SQL_Subqueries.sql
@@ -35,6 +35,13 @@ WHERE address_ID in (SELECT address_ID from address
 					WHERE city_ID in (SELECT city_ID from city
 					WHERE country_ID in (SELECT country_ID from country
                     WHERE country = "Canada")));
+                    
+SELECT first_name, last_name, email from customer
+JOIN address USING (address_ID)
+JOIN city USING (city_ID)
+JOIN country USING (country_ID)
+WHERE country = "Canada"
+GROUP BY first_name, last_name, email;
 
 # 6) Optional: Which are films starred by the most prolific actor? Most prolific actor is defined as the actor that has acted in the most number of films. First you will have to find the most prolific actor and then use that actor_id to find the different films that he/she starred.
 
@@ -43,41 +50,33 @@ WHERE film_id in (SELECT film_id from film_actor
 					WHERE actor_id = (SELECT actor_ID from film_actor
                     GROUP BY actor_id ORDER BY count(film_id) DESC LIMIT 1));
 
-SELECT title, actor_id from film
-JOIN film_actor USING (film_ID)
-JOIN actor USING (actor_ID)
-GROUP BY actor_id
-ORDER BY count(film_ID) DESC LIMIT 1;
+
+# 7) Films rented by most profitable customer. You can use the customer table and payment table to find the most profitable customer ie the customer that has made the largest sum of payments
+USE sakila;
+SELECT title from film
+WHERE film_id in (SELECT film_id from inventory
+					WHERE inventory_id in (SELECT inventory_id from rental
+                    WHERE customer_id = (SELECT customer_id from payment
+                    GROUP BY customer_id 
+                    ORDER BY sum(amount) DESC LIMIT 1)));
 
 
+# 8) Customers who spent more than the average payments(this refers to the average of all amount spent per each customer).
+#Solution from Peter Zimmermann PZ
+CREATE TEMPORARY table temp_tt
+SELECT ..
 
-# 7)Films rented by most profitable customer. You can use the customer table and payment table to find the most profitable customer ie the customer that has made the largest sum of payments
-SELECT title from film;
-WHERE film_id in (SELECT film_id from film_actor
-					WHERE actor_id = (SELECT actor_ID from film_actor
-                    GROUP BY actor_id ORDER BY count(film_id) DESC LIMIT 1));
+SELECT sum(amount) as total_paid, customer_id from payment
+GROUP BY customer_id
+SELECT avg(total_paid) as avg_total_paid
+FROM (Select sum(amount) as total_paid from payment
+GROUP BY customer_id) as sub;
 
-Expected output:
+SELECT first_name, last_name, sum(amount) as total_paid, customer_id
+FROM payment
+JOIN customer USING (customer_id)
+GROUP BY customer_ID
+HAVING total_paid > (SELECT avg(total_paid) FROM (SELECT sum(amount) as total_paid FROM payment 
+GROUP BY customer_id) as sub)
+ORDER BY first_name, last_name DESC;
 
-44 rows including 
-DESTINY SATURDAY
-CYCLONE FAMILY
-SLUMS DUCK
-FIDELITY DEVIL
-SPLASH GUMP
-MISSION ZOOLANDER
-MULHOLLAND BEAST
-PRINCESS GIANT
-PARIS WEEKEND
-RACER EGG
-Customers who spent more than the average payments(this refers to the average of all amount spent per each customer).
-Expected output:
-
-285 rows including:
-ALAN KAHN
-ALEX GRESHAM
-ALEXANDER FENNELL
-…
-WARREN SHERROD
-WESLEY BULL
-ZACHARY HITE
